4e5d224db203b39746a820788bf139b0e2387ee0
chite <105101021@nccu.edu.tw>
2019-03-31 14:40:22 +0800

    decrease global variable

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,16 +1,19 @@
-let card_arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-let card_status = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let turn = 0;
-let firstNum = 0;
-let secondNum = 0;
-let firstIndex = 0;
-let secondIndex = 0;
-let counter = 0;
-let finish = 0;
-let inter;
+let glo = {
+	card_arr: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8],
+	card_status: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	turn: 0,
+	firstNum: 0,
+	secondNum: 0,
+	firstIndex: 0,
+	secondIndex: 0,
+	counter: 0,
+	finish: 0,
+	inter: null
+};
+
 
 //initialize
-change(card_arr);
+change(glo.card_arr);
 $('.bor').hide();
 $('section').fadeOut(0);
 $(".img-element").each(
@@ -29,11 +32,11 @@ $(".img-element").on("click", clickCard);
 $('.start').on('click', function(e) {
     if ($('.sec').text() == 0) {
         $('.start').blur().attr('disabled', true);
-        inter = setInterval(function() {
+        glo.inter = setInterval(function() {
             if (!$('.start').hasClass('pauseInterval')) {
-                counter++;
+                glo.counter++;
             }
-            $('.sec').text(counter);
+            $('.sec').text(glo.counter);
         }, 1000);
         $('.pause').prop('disabled', false);
     } else {
@@ -61,62 +64,62 @@ function change(arr) {
 }
 
 function clickCard() {
-    if (turn != 2 && counter != 0 && !$('.pause').is(':disabled')) {
+    if (glo.turn != 2 && glo.counter != 0 && !$('.pause').is(':disabled')) {
         let elementIndex = $(".img-element").index(this);
-        if (card_status[elementIndex] != 1) {
+        if (glo.card_status[elementIndex] != 1) {
             $(".img-element").eq(elementIndex).addClass('flip');
-            setTimeout(() => $(".img-element").eq(elementIndex).attr("src", "resources/" + card_arr[elementIndex] + ".png").removeClass('flip'), 500);
-            card_status[elementIndex] = 1;
-            if (firstNum == 0) {
-                firstNum = card_arr[elementIndex];
-                firstIndex = elementIndex;
-                turn = 1;
+            setTimeout(() => $(".img-element").eq(elementIndex).attr("src", "resources/" + glo.card_arr[elementIndex] + ".png").removeClass('flip'), 500);
+            glo.card_status[elementIndex] = 1;
+            if (glo.firstNum == 0) {
+                glo.firstNum = glo.card_arr[elementIndex];
+                glo.firstIndex = elementIndex;
+                glo.turn = 1;
             } else {
-                secondNum = card_arr[elementIndex];
-                secondIndex = elementIndex;
-                turn = 2;
-                if (firstNum != secondNum) {
+                glo.secondNum = glo.card_arr[elementIndex];
+                glo.secondIndex = elementIndex;
+                glo.turn = 2;
+                if (glo.firstNum != glo.secondNum) {
                     setTimeout(function() {
-                        $(".img-element").eq(firstIndex).addClass('flip');
-                        $(".img-element").eq(secondIndex).addClass('flip');
+                        $(".img-element").eq(glo.firstIndex).addClass('flip');
+                        $(".img-element").eq(glo.secondIndex).addClass('flip');
                         setTimeout(function() {
-                            $(".img-element").eq(firstIndex).attr("src", "resources/bg_card.png").removeClass('flip');
-                            $(".img-element").eq(secondIndex).attr("src", "resources/bg_card.png").removeClass('flip');
-                            card_status[firstIndex] = 0;
-                            card_status[secondIndex] = 0;
-                            firstNum = 0;
-                            secondNum = 0;
-                            firstIndex = 0;
-                            secondIndex = 0;
-                            turn = 0;
+                            $(".img-element").eq(glo.firstIndex).attr("src", "resources/bg_card.png").removeClass('flip');
+                            $(".img-element").eq(glo.secondIndex).attr("src", "resources/bg_card.png").removeClass('flip');
+                            glo.card_status[glo.firstIndex] = 0;
+                            glo.card_status[glo.secondIndex] = 0;
+                            glo.firstNum = 0;
+                            glo.secondNum = 0;
+                            glo.firstIndex = 0;
+                            glo.secondIndex = 0;
+                            glo.turn = 0;
                         }, 500);
                     }, 2000);
                 } else {
-                    $.each(card_status, function(index, val) {
+                    $.each(glo.card_status, function(index, val) {
                         if (val == 1) {
-                            finish++;
+                            glo.finish++;
                         }
                     });
-                    $("section").eq(firstNum - 1).fadeIn(3000); //跑出文字
+                    $("section").eq(glo.firstNum - 1).fadeIn(3000); //跑出文字
                     setTimeout(function() { //轉正面
                         //eyes
-                        if (finish != 16 && firstNum == 8) {
+                        if (glo.finish != 16 && glo.firstNum == 8) {
                             eyes();
                         }
                         //finish
-                        if (finish == 16) { //結束
-                            clearInterval(inter);
+                        if (glo.finish == 16) { //結束
+                            clearInterval(glo.inter);
                             $('.pause').prop('disabled', true);
                             $('.start').prop('disabled', false).text("重新開始").on('click', function() {
                                 location.reload();
                             });
                         }
-                        firstNum = 0;
-                        secondNum = 0;
-                        firstIndex = 0;
-                        secondIndex = 0;
-                        turn = 0;
-                        finish = 0;
+                        glo.firstNum = 0;
+                        glo.secondNum = 0;
+                        glo.firstIndex = 0;
+                        glo.secondIndex = 0;
+                        glo.turn = 0;
+                        glo.finish = 0;
                     }, 1000);
                 }
             }
@@ -128,14 +131,14 @@ function eyes() {
     $('.bor').show();
     $('.img-element').each(function(i) {
         $(this).css("pointer-events", "none");
-        if (card_status[i] == 0) {
-            $(this).attr("src", "resources/" + card_arr[i] + ".png").css({ "filter": "blur(2px)" });
+        if (glo.card_status[i] == 0) {
+            $(this).attr("src", "resources/" + glo.card_arr[i] + ".png").css({ "filter": "blur(2px)" });
         }
     });
     setTimeout(function() {
         $('.img-element').each(function(i) {
             $(this).css("pointer-events", "auto");
-            if (card_status[i] == 0) {
+            if (glo.card_status[i] == 0) {
                 $(this).attr("src", "resources/bg_card.png").css({ "filter": "none" });
             }
         });
